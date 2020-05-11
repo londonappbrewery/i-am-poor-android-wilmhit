b77a863a54f22a4ba0525bafff86546610c284f9
Wilmhit <fakemail@mail>
2020-05-11 11:22:01 +0200

    Working app

--- app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.wilmhit.iampoor.R;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
